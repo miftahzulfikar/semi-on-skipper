c1aa706c0d750db1bb75ece9566e71b0e28c584a
miftah <miftah.zulfikar@tokopedia.com>
2024-03-27 11:01:45 +0700

    add custom theme and dark mode

--- skipper.config.js
+++ skipper.config.js
@@ -1,3 +1,16 @@
 const define = require('@tokopedia/skipper/cli/config/define-config');
+const SemiWebpackPlugin = require('@douyinfe/semi-webpack-plugin').default;
 
-module.exports = define();
+module.exports = define({
+  webpack(config) {
+    const options = config;
+
+    options.plugins = config.plugins.concat(
+      new SemiWebpackPlugin({
+        theme: '@semi-bot/semi-theme-tuxsemi',
+      }),
+    );
+
+    return options;
+  },
+});
--- src/routes/Semi/index.tsx
+++ src/routes/Semi/index.tsx
@@ -14,6 +14,15 @@ import { Button, Space } from '@douyinfe/semi-ui';
 const Semi: SkipperComponentType = () => {
   const navigate = useNavigate();
 
+  const switchMode = () => {
+    const body = document.body;
+    if (body.hasAttribute('theme-mode')) {
+      body.removeAttribute('theme-mode');
+    } else {
+      body.setAttribute('theme-mode', 'dark');
+    }
+  };
+
   return (
     <Main>
       <Helmet>
@@ -30,6 +39,7 @@ const Semi: SkipperComponentType = () => {
         <p className={styles.description}>Test Semi design on skipper framework</p>
 
         <h2>Button</h2>
+        <p>Light Button (default)</p>
         <Space>
           <Button>Primary Button</Button>
           <Button type="secondary">Secondary Button</Button>
@@ -37,6 +47,25 @@ const Semi: SkipperComponentType = () => {
           <Button type="warning">Warning Button</Button>
           <Button type="danger">Danger Button</Button>
         </Space>
+        <p>Solid Button</p>
+        <Space>
+          <Button theme="solid">Primary Button</Button>
+          <Button theme="solid" type="secondary">
+            Secondary Button
+          </Button>
+          <Button theme="solid" type="tertiary">
+            Tertiary Button
+          </Button>
+          <Button theme="solid" type="warning">
+            Warning Button
+          </Button>
+          <Button theme="solid" type="danger">
+            Danger Button
+          </Button>
+        </Space>
+
+        <h2>Light / Dark Mode</h2>
+        <Button onClick={switchMode}>Switch Mode</Button>
       </div>
     </Main>
   );
